564e09af8af431d3e619ab82f83fbc60ffc6f325
mj <85698909+mehul-clou@users.noreply.github.com>
2023-12-17 15:24:37 +0000

    Update Player.jsx
    Create two State === one is set name add another is to set button .

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enterPlayerName, setEnterPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setSubmitted(false);
+    setEnterPlayerName(e.target.value);
+  }
+
+  function handleClick(e) {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
